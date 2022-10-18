45ac1d9f37058c526c6de5bb3bcdf44bb7937b47
Wisnu Box <hwisnu222@gmail.com>
2022-10-18 16:44:14 +0700

    Fix syntax unused

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { convertDate } from "../utils/convertDate";
